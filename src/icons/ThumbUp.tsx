2b84cc1aa1271856274613eaf213eddecc935a43
Megrax <geronimooo@foxmail.com>
2021-09-02 22:32:00 +0800

    :ambulance: fix(attempt): prod svg error

--- src/icons/ThumbUp.tsx
+++ src/icons/ThumbUp.tsx
@@ -5,7 +5,8 @@ export default function ThumbUp(props: { size: number }) {
 
 	return (
 		<svg
-			className={`w-${size} h-${size}`}
+			// className={`w-${size} h-${size}`}
+			width="20"
 			viewBox="0 0 48 48"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
